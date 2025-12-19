66aede877a19c189071550895dc9e7bd88ee62a7
DouglasRJ <dgrenato@outlook.com>
2025-12-19 10:22:17 -0300

    Add UUID extension in user migration

--- apps/api/src/database/migrations/1765986163034-user.ts
+++ apps/api/src/database/migrations/1765986163034-user.ts
@@ -4,6 +4,8 @@ export class User1765986163034 implements MigrationInterface {
   name = 'User1765986163034';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
     await queryRunner.query(
       `CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
     );
